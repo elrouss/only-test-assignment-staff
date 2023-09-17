186ed441044817ecaeef1f62a7de9c7f1f0e9fe3
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-09-17 15:06:25 +0300

    feat: describe component default heading

---
+++ src/components/headings/heading-default/heading-default.tsx
@@ -0,0 +1,55 @@
+import classNames from 'classnames';
+
+import styles from './heading-paragraph.module.scss';
+
+interface IHeadingDefault {
+  level: 2 | 3 | 4 | 5 | 6;
+  type: 'section' | 'paragraph';
+  text: string;
+  color: 'lightBlue' | 'darkBlue';
+  hasUppercase?: boolean;
+}
+
+export const HeadingDefault = ({
+  level,
+  type,
+  text,
+  color,
+  hasUppercase = false,
+}: IHeadingDefault) => {
+  const selectors = classNames(styles[type], styles[color], {
+    [styles.uppercase]: hasUppercase,
+  });
+
+  let heading:
+    | undefined
+    | React.ReactElement<
+        React.DetailedHTMLProps<
+          React.HTMLAttributes<HTMLHeadingElement>,
+          HTMLHeadingElement
+        >
+      >;
+
+  switch (level) {
+    case 2:
+      heading = <h2 className={selectors}>{text}</h2>;
+      break;
+
+    case 3:
+      heading = <h3 className={selectors}>{text}</h3>;
+      break;
+
+    case 4:
+      heading = <h4 className={selectors}>{text}</h4>;
+      break;
+
+    case 5:
+      heading = <h5 className={selectors}>{text}</h5>;
+      break;
+
+    default:
+      heading = <h6 className={selectors}>{text}</h6>;
+  }
+
+  return heading;
+};
