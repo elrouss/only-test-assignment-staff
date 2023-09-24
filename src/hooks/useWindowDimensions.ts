ebdf45deb0e687fe6e788fb12f44aef9dd873d79
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-09-24 15:42:13 +0300

    feat: add deploy on gh pages

--- src/hooks/useWindowDimensions.ts
+++ src/hooks/useWindowDimensions.ts
@@ -16,7 +16,7 @@ export const useWindowDimensions = () => {
     }
 
     window.addEventListener('resize', handleResize);
-  
+
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
